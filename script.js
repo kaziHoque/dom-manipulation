2f748a5891af6b6f1f792884d83534646b2c91c6
Scarlet McLearn <ratul_shams@yahoo.com>
2018-10-10 03:12:20 +0000

    g

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ console.log(heading.innerText);
 
 heading.innerText = '<h3>Asa Cat!</h3>'
 
-/* Task 2 using plain JavaScript */
 
+/* Task 2 using plain JavaScript */
+var textBtn = document.getElementById('addText2');
+textBtn.addEventListener('click', function(event){
+    var task2 = document.getElementById('task2a');
+    
+    var paragraph = document.createElement('p');
+     
+    paragraph.innerText = 'Hello World!';
+    
+    task2.appendChild(paragraph);
+})
 
 
 /* Task 4 using jQuery */
